cab42dc77aa59c1ac51f7528a1633dd36e915f3b
abdipriyangga <mabdipme@gmail.com>
2023-09-05 07:22:00 +0700

    add package nodemon

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT;
 const mainRouter = require("./src/routes/index");
-
+const cors = require('cors');
 app.get('/hello', (req, res) => {
   res.set('Content-Type', 'text/html');
   res.status(200).send("<h1>Hello GFG Learner!</h1>");
 });
 app.use(express.json());
 app.use(express.urlencoded());
-
+app.use(cors());
 app.listen(PORT, (error) => {
   if (!error)
     console.log("Server is Successfully Running, and App is listening on port " + PORT)
